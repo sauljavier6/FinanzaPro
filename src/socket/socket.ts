67355b1757f4da985af090197c3687f6ab8592f5
saul peralta <psauljavier6@gmail.com>
2026-06-30 12:29:23 -0700

    Ajuste sistema de notificaciones e historial

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
+console.log("Socket URL:", import.meta.env.VITE_BACKEND_URL);
+
 export const socket = io(import.meta.env.VITE_BACKEND_URL, {
   withCredentials: true,
 });
